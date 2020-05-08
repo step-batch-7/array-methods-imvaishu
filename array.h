5ff623044b58aceec41f75965ac86a3a6a1dd2f7
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2020-05-08 19:19:29 +0530

    typedef different pointers

--- array.h
+++ array.h
@@ -1,10 +1,14 @@
 #ifndef __ARRAY_H
 #define __ARRAY_H
+
 typedef enum
 {
   False,
   True
 } Bool;
+
+typedef int *Int_ptr; 
+typedef Array *Array_ptr; 
 
 typedef int (*Mapper)(int);
 typedef Bool (*Predicate)(int);
@@ -12,12 +16,12 @@ typedef int (*Reducer)(int, int);
 
 typedef struct
 {
-  int *array;
+  Int_ptr array;
   int length;
 } Array;
 
-Array *map(Array *src, Mapper mapper);
-Array *filter(Array *src, Predicate predicate);
-int reduce(Array *src, int init, Reducer reducer);
+Array_ptr map(Array_ptr src, Mapper mapper);
+Array_ptr filter(Array_ptr src, Predicate predicate);
+int reduce(Array_ptr src, int init, Reducer reducer);
 
 #endif
