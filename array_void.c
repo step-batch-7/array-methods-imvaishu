32c198f55e32b495e90d8d64cc4121ce263c63a7
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2020-05-09 20:49:22 +0530

    changed loop from for to while in map function

--- array_void.c
+++ array_void.c
@@ -13,12 +13,13 @@ ArrayVoid_ptr map_void(ArrayVoid_ptr src, MapperVoid mapper)
 {
   ArrayVoid_ptr new_array = malloc(sizeof(ArrayVoid));
   new_array->array =  malloc(sizeof(Object) * src->length);
-  
-  for(int i = 0 ; i < src->length ; i++)
+  int count = 0;
+  while(count < src->length)
   {
-    new_array->array[i] = mapper(src->array[i]);
+    new_array->array[count] = mapper(src->array[count]);
+    count++;
   }
-  new_array->length = src->length;
+  new_array->length = count;
   return new_array;
 }
 
